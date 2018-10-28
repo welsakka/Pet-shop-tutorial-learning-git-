8bb0494ae43aa43bcf63667496302d7e1dd77fd2
Waleed Elsakka <waleedpahome@live.com>
2018-10-28 17:02:11 -0400

    The REAL change

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -3,19 +3,18 @@ pragma solidity ^0.4.24;
 contract Adoption {
 
 	address[16] public adopters;
-
+	
 	function adopt(uint petId) public returns (uint) {
-
+        
         require(petId >= 0 && petId <= 15);
         adopters[petId] = msg.sender;
-				//REverted back to the old way
+		sender I am the message;
 
     	return petId;
 	}
-
+	
 	//retrieves adopters
 	function getAdopters() public view returns (address[16]) {
 	    return adopters;
 	}
-
 }
