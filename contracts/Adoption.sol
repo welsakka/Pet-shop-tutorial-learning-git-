6c443609d7669249c9902f43c2d8f83e12e0cd1b
Waleed Elsakka <waleedpahome@live.com>
2018-10-28 16:50:56 -0400

    Testing stuff i guess

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -3,18 +3,19 @@ pragma solidity ^0.4.24;
 contract Adoption {
 
 	address[16] public adopters;
-	
+
 	function adopt(uint petId) public returns (uint) {
-        
+
         require(petId >= 0 && petId <= 15);
         adopters[petId] = msg.sender;
+				msg.sender = sender.msg,no
 
-        return petId;
+    	return petId;
 	}
-	
+
 	//retrieves adopters
 	function getAdopters() public view returns (address[16]) {
 	    return adopters;
 	}
-	
+
 }
