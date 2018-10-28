713a598b84f329081a1d65bb08fae344cd84e5db
Waleed Elsakka <waleedpahome@live.com>
2018-10-27 21:32:54 -0400

    initial project version

---
+++ contracts/Adoption.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.24;
+
+contract Adoption {
+
+	address[16] public adopters;
+	
+	function adopt(uint petId) public returns (uint) {
+        
+        require(petId >= 0 && petId <= 15);
+        adopters[petId] = msg.sender;
+
+        return petId;
+	}
+	
+	//retrieves adopters
+	function getAdopters() public view returns (address[16]) {
+	    return adopters;
+	}
+	
+}
---
+++ test/TestAdoption.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.24;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Adoption.sol";
+
+contract TestAdoption {
+    Adoption adoption = Adoption(DeployedAddresses.Adoption());
+    
+    function testUserCanAdoptPet() public {
+	
+        uint returnedId = adoption.adopt(8);
+        uint expected = 8;
+
+        Assert.equal(returnedId, expected, "Adoption of pet ID 8 should be recorded.");
+    }
+
+	function testGetSingleAdopter() public {
+		//Expected owner is this contract
+		address expected = this;
+		
+		address adopter = adoption.adopters(8);
+		
+		Assert.equal(adopter, expected, "Address of ID 8 should be the same");
+	}		
+	
+	function testReturnArray() public {
+	
+		address expected = this;
+	
+		//store contract in memory rather than contract's storage
+		address[16] memory adopters = adoption.getAdopters();
+	
+		Assert.equal(adopters[8], expected, "Owner of pet ID 8 should be recorded");
+    }
+}
